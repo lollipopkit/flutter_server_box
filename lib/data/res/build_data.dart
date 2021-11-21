de1e9701082c2a7ca33bbfa5ee09148060c11a93
LollipopKit <2036293523@qq.com>
2021-11-21 19:45:42 +0800

    Add r/w permission

--- lib/data/res/build_data.dart
+++ lib/data/res/build_data.dart
@@ -2,8 +2,8 @@
 
 class BuildData {
   static const String name = "ToolBox";
-  static const int build = 63;
+  static const int build = 64;
   static const String engine = "Flutter 2.5.3 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision 18116933e7 (5 weeks ago) • 2021-10-15 10:46:35 -0700\nEngine • revision d3ea636dc5\nTools • Dart 2.14.4\n";
-  static const String buildAt = "2021-11-21 19:35:22.997443";
-  static const int modifications = 4;
+  static const String buildAt = "2021-11-21 19:42:23.223010";
+  static const int modifications = 2;
 }
