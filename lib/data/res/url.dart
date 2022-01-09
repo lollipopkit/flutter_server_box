317936c521aff5c829743dab426f576a1a92b1c1
Junyuan Feng <lollipopkit@qq.com>
2022-01-09 18:18:06 +0800

    Change url

--- lib/data/res/url.dart
+++ lib/data/res/url.dart
@@ -1,5 +1,5 @@
-const backendUrl = 'https://v2.custed.lolli.tech';
-const baseUrl = backendUrl + '/res/toolbox';
+const backendUrl = 'https://res.lolli.tech';
+const baseUrl = backendUrl + '/toolbox';
 const joinQQGroupUrl = 'https://jq.qq.com/?_wv=1027&k=G0hUmPAq';
 const myGithub = 'https://github.com/LollipopKit';
 const rainSunMeGithub = 'https://github.com/RainSunMe';
