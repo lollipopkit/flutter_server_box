521ff998af5bb6a9eee17eb208fda0c60c3f7afd
Junyuan Feng <lollipopkit@qq.com>
2022-05-04 21:08:03 +0800

    Empty.

--- lib/data/res/build_data.dart
+++ lib/data/res/build_data.dart
@@ -2,9 +2,9 @@
 
 class BuildData {
   static const String name = "ServerBox";
-  static const int build = 120;
+  static const int build = 122;
   static const String engine =
-      "Flutter 2.10.4 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision c860cba910 (6 weeks ago) • 2022-03-25 00:23:12 -0500\nEngine • revision 57d3bac3dd\nTools • Dart 2.16.2 • DevTools 2.9.2\n";
-  static const String buildAt = "2022-05-04 19:40:22.762092";
-  static const int modifications = 0;
+      "Flutter 2.10.5 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision 5464c5bac7 (2 weeks ago) • 2022-04-18 09:55:37 -0700\nEngine • revision 57d3bac3dd\nTools • Dart 2.16.2 • DevTools 2.9.2\n";
+  static const String buildAt = "2022-05-04 21:07:15.254894";
+  static const int modifications = 5;
 }
