e423e5615250435ea03a0bbf2f0dd3346be9a717
lollipopkit <lollipopkit@qq.com>
2023-02-18 13:18:35 +0800

    try solve `file_picker`

--- lib/data/res/build_data.dart
+++ lib/data/res/build_data.dart
@@ -2,8 +2,8 @@
 
 class BuildData {
   static const String name = "ServerBox";
-  static const int build = 223;
-  static const String engine = "Flutter 3.7.3 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision 9944297138 (8 days ago) • 2023-02-08 15:46:04 -0800\nEngine • revision 248290d6d5\nTools • Dart 2.19.2 • DevTools 2.20.1\n";
-  static const String buildAt = "2023-02-17 18:51:44.086252";
-  static const int modifications = 7;
+  static const int build = 224;
+  static const String engine = "Flutter 3.7.3 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision 9944297138 (9 days ago) • 2023-02-08 15:46:04 -0800\nEngine • revision 248290d6d5\nTools • Dart 2.19.2 • DevTools 2.20.1\n";
+  static const String buildAt = "2023-02-18 13:16:46.172437";
+  static const int modifications = 1;
 }
