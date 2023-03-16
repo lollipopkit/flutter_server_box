bb15f346157ee7233d5ea51b75f3b3730d99f762
lollipopkit <lollipopkit@qq.com>
2023-03-16 11:47:06 +0800

    #17 ios production env no `token`

--- lib/data/res/build_data.dart
+++ lib/data/res/build_data.dart
@@ -2,9 +2,8 @@
 
 class BuildData {
   static const String name = "ServerBox";
-  static const int build = 231;
-  static const String engine =
-      "Flutter 3.7.6 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision 12cb4eb7a0 (11 days ago) • 2023-03-01 10:29:26 -0800\nEngine • revision ada363ee93\nTools • Dart 2.19.3 • DevTools 2.20.1\n";
-  static const String buildAt = "2023-03-13 11:33:00.843861";
-  static const int modifications = 13;
+  static const int build = 233;
+  static const String engine = "Flutter 3.7.7 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision 2ad6cd72c0 (7 days ago) • 2023-03-08 09:41:59 -0800\nEngine • revision 1837b5be5f\nTools • Dart 2.19.4 • DevTools 2.20.1\n";
+  static const String buildAt = "2023-03-15 23:00:21.075300";
+  static const int modifications = 2;
 }
