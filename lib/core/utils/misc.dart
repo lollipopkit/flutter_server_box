f3ceb73f0ef82acd81627dbc2b1037237d6a397b
lollipopkit <10864310+lollipopkit@users.noreply.github.com>
2024-06-11 16:40:05 +0800

    fix: rm `gms` (#379 #381)

--- lib/core/utils/misc.dart
+++ lib/core/utils/misc.dart
@@ -2,14 +2,9 @@ import 'package:fl_lib/fl_lib.dart';
 import 'package:plain_notification_token/plain_notification_token.dart';
 
 Future<String?> getToken() async {
-  if (isIOS) {
-    final plainNotificationToken = PlainNotificationToken();
-    plainNotificationToken.requestPermission();
-
-    // If you want to wait until Permission dialog close,
-    // you need wait changing setting registered.
-    await plainNotificationToken.onIosSettingsRegistered.first;
-    return await plainNotificationToken.getToken();
-  }
-  return null;
+  if (!isIOS) return null;
+  final instance = ApnsToken()..requestPermission();
+  // Wait until Permission dialog closed
+  await instance.onIosSettingsRegistered.first;
+  return await instance.getToken();
 }
