108d0a5a5b4a6889ffbc801df4a95142910c49d2
lollipopkit <10864310+lollipopkit@users.noreply.github.com>
2024-06-13 00:45:24 +0800

    chore: bump version
    Signed-off-by: lollipopkit <10864310+lollipopkit@users.noreply.github.com>

--- lib/data/res/build_data.dart
+++ lib/data/res/build_data.dart
@@ -2,9 +2,9 @@
 
 class BuildData {
   static const String name = "ServerBox";
-  static const int build = 967;
+  static const int build = 970;
   static const String engine = "3.22.2";
-  static const String buildAt = "2024-06-12 21:30:20";
-  static const int modifications = 3;
+  static const String buildAt = "2024-06-13 00:44:23";
+  static const int modifications = 0;
   static const int script = 49;
 }
