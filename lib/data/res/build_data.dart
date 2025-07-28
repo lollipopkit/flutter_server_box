46a12bc84488da8e2df6a91efed694a359ed0953
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2025-07-28 22:27:56 +0800

    bump: `v1201`

--- lib/data/res/build_data.dart
+++ lib/data/res/build_data.dart
@@ -3,6 +3,6 @@
 
 abstract class BuildData {
   static const String name = "ServerBox";
-  static const int build = 1195;
-  static const int script = 64;
+  static const int build = 1201;
+  static const int script = 65;
 }
