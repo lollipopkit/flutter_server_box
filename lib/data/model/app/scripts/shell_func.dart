07590b4ba31d921f03558cd25724e851b66a1371
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2025-08-08 18:43:03 +0800

    opt.

--- lib/data/model/app/scripts/shell_func.dart
+++ lib/data/model/app/scripts/shell_func.dart
@@ -6,7 +6,7 @@ import 'package:server_box/data/provider/server.dart';
 /// Shell functions available in the ServerBox application
 enum ShellFunc {
   status('SbStatus'),
-  process('SbProcess'), 
+  process('SbProcess'),
   shutdown('SbShutdown'),
   reboot('SbReboot'),
   suspend('SbSuspend');
@@ -19,7 +19,7 @@ enum ShellFunc {
   /// Get the command line flag for this function
   String get flag => switch (this) {
     ShellFunc.process => 'p',
-    ShellFunc.shutdown => 'sd', 
+    ShellFunc.shutdown => 'sd',
     ShellFunc.reboot => 'r',
     ShellFunc.suspend => 'sp',
     ShellFunc.status => 's',
@@ -30,83 +30,73 @@ enum ShellFunc {
     final scriptPath = ShellFuncManager.getScriptPath(id, systemType: systemType);
     final isWindows = systemType == SystemType.windows;
     final builder = ScriptBuilderFactory.getBuilder(isWindows);
-    
+
     return builder.getExecCommand(scriptPath, this);
   }
-
 }
 
 /// Manager class for shell function operations
 class ShellFuncManager {
   const ShellFuncManager._();
-  
-  /// Normalize a directory path to ensure it doesn't end with a trailing separator
+
+  /// Normalize a directory path to ensure it doesn't end with trailing separators
   static String _normalizeDir(String dir, bool isWindows) {
-    final separator = isWindows 
-        ? ScriptConstants.windowsPathSeparator 
-        : ScriptConstants.unixPathSeparator;
-    
-    if (dir.endsWith(separator)) {
-      return dir.substring(0, dir.length - 1);
-    }
-    return dir;
+    final separator = isWindows ? ScriptConstants.windowsPathSeparator : ScriptConstants.unixPathSeparator;
+
+    // Remove all trailing separators
+    final pattern = RegExp('${RegExp.escape(separator)}+\$');
+    return dir.replaceAll(pattern, '');
   }
-  
+
   /// Get the script directory for the given [id].
   ///
   /// Checks for custom script directory first, then falls back to default.
   static String getScriptDir(String id, {SystemType? systemType}) {
     final customScriptDir = ServerProvider.pick(id: id)?.value.spi.custom?.scriptDir;
-    if (customScriptDir != null) return customScriptDir;
-    
     final isWindows = systemType == SystemType.windows;
+
+    if (customScriptDir != null) return _normalizeDir(customScriptDir, isWindows);
     return ScriptPaths.getScriptDir(id, isWindows: isWindows);
   }
-  
+
   /// Switch between tmp and home directories for script storage
   static void switchScriptDir(String id, {SystemType? systemType}) {
     final isWindows = systemType == SystemType.windows;
     ScriptPaths.switchScriptDir(id, isWindows: isWindows);
   }
-  
+
   /// Get the full script path for the given [id]
   static String getScriptPath(String id, {SystemType? systemType}) {
     final customScriptDir = ServerProvider.pick(id: id)?.value.spi.custom?.scriptDir;
     if (customScriptDir != null) {
       final isWindows = systemType == SystemType.windows;
       final normalizedDir = _normalizeDir(customScriptDir, isWindows);
-      final fileName = isWindows 
-          ? ScriptConstants.scriptFileWindows 
-          : ScriptConstants.scriptFile;
-      final separator = isWindows 
-          ? ScriptConstants.windowsPathSeparator 
-          : ScriptConstants.unixPathSeparator;
+      final fileName = isWindows ? ScriptConstants.scriptFileWindows : ScriptConstants.scriptFile;
+      final separator = isWindows ? ScriptConstants.windowsPathSeparator : ScriptConstants.unixPathSeparator;
       return '$normalizedDir$separator$fileName';
     }
-    
+
     final isWindows = systemType == SystemType.windows;
     return ScriptPaths.getScriptPath(id, isWindows: isWindows);
   }
-  
+
   /// Get the installation shell command for the script
   static String getInstallShellCmd(String id, {SystemType? systemType}) {
     final scriptDir = getScriptDir(id, systemType: systemType);
     final isWindows = systemType == SystemType.windows;
     final normalizedDir = _normalizeDir(scriptDir, isWindows);
     final builder = ScriptBuilderFactory.getBuilder(isWindows);
-    final separator = isWindows 
-        ? ScriptConstants.windowsPathSeparator 
-        : ScriptConstants.unixPathSeparator;
+    final separator = isWindows ? ScriptConstants.windowsPathSeparator : ScriptConstants.unixPathSeparator;
     final scriptPath = '$normalizedDir$separator${builder.scriptFileName}';
-    
+
     return builder.getInstallCommand(normalizedDir, scriptPath);
   }
-  
+
   /// Generate complete script based on system type
   static String allScript(Map<String, String>? customCmds, {SystemType? systemType}) {
     final isWindows = systemType == SystemType.windows;
     final builder = ScriptBuilderFactory.getBuilder(isWindows);
-    
+
     return builder.buildScript(customCmds);
   }
 }
