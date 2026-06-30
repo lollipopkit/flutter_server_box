82bf08a3176764bf84bd4e866444118a6b594c9e
GT610 <79314033+GT-610@users.noreply.github.com>
2026-06-30 11:21:15 +0800

    Switch update checks to GitHub releases (#1219)

--- lib/data/res/url.dart
+++ lib/data/res/url.dart
@@ -1,8 +1,10 @@
 abstract final class Urls {
   static const cdnBase = 'https://cdn.lpkt.cn/serverbox';
-  static const updateCfg = '$cdnBase/update2.json';
   static const myGithub = 'https://github.com/lollipopkit';
+  static const githubApi = 'https://api.github.com/repos/lollipopkit';
   static const thisRepo = '$myGithub/flutter_server_box';
+  static const githubReleasesApi = '$githubApi/flutter_server_box/releases';
+  static const appStore = 'https://apps.apple.com/app/id1586449703';
   static const appHelp = '$thisRepo#-help';
   static const appWiki = '$thisRepo/wiki';
 }
