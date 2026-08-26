f7542be3c582b0832308d65669e8037d5ea05981
GT610 <myddz1005@163.com>
2026-08-26 20:23:50 +0800

    Exclude Android backup and device transfer data

--- test/android_backup_policy_test.dart
+++ test/android_backup_policy_test.dart
@@ -4,18 +4,54 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   const manifest = 'android/app/src/main/AndroidManifest.xml';
-  const obsoleteRules = 'android/app/src/main/res/xml/backup_rules.xml';
+  const backupRules = 'android/app/src/main/res/xml/backup_rules.xml';
+  const dataExtractionRules =
+      'android/app/src/main/res/xml/data_extraction_rules.xml';
+  const excludedDomains = [
+    'root',
+    'file',
+    'database',
+    'sharedpref',
+    'external',
+  ];
+
+  String section(String source, String tag) {
+    final match = RegExp(
+      '<$tag(?:\\s[^>]*)?>([\\s\\S]*?)</$tag>',
+    ).firstMatch(source);
+    expect(match, isNotNull, reason: 'missing <$tag>');
+    return match!.group(1)!;
+  }
+
+  void expectAllDomainsExcluded(String source) {
+    for (final domain in excludedDomains) {
+      expect(source, contains('<exclude domain="$domain" path="." />'));
+    }
+  }
 
   test('Android system backup stays disabled', () {
     final source = File(manifest).readAsStringSync();
 
     expect(source, contains('android:allowBackup="false"'));
-    expect(source, isNot(contains('android:fullBackupContent=')));
-    expect(source, isNot(contains('android:dataExtractionRules=')));
+    expect(source, contains('android:fullBackupContent="@xml/backup_rules"'));
+    expect(
+      source,
+      contains('android:dataExtractionRules="@xml/data_extraction_rules"'),
+    );
+    expect(source, contains('android:hasFragileUserData="true"'));
     expect(source, isNot(contains('android:restoreAnyVersion=')));
   });
 
-  test('no obsolete backup rules suggest the database is transferable', () {
-    expect(File(obsoleteRules).existsSync(), isFalse);
+  test('Android 11 and earlier backup rules exclude every app data domain', () {
+    final source = File(backupRules).readAsStringSync();
+
+    expectAllDomainsExcluded(section(source, 'full-backup-content'));
+  });
+
+  test('Android 12 rules exclude cloud backup and device transfer', () {
+    final source = File(dataExtractionRules).readAsStringSync();
+
+    expectAllDomainsExcluded(section(source, 'cloud-backup'));
+    expectAllDomainsExcluded(section(source, 'device-transfer'));
   });
 }
